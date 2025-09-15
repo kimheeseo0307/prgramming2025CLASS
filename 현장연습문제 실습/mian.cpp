e744e57fb7b12f91c46f77bb52cf5febdd418e8d
HEESEO KIM <01024915177kim@gmail.com>
2025-09-15 16:41:43 +0900

    현장실습
    9/15 실습

---
+++ 현장연습문제 실습/mian.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("=== Basic Constants === \n\n");
+	printf("Nmae\t : %s\n\n", "heeseo");
+	printf("Initial\t : %c\n\n", 'K');
+	printf("Age : %d\n\n", 23);
+	printf("Score\t : %.6f\n\n", 93.5);
+	printf("Count\t : %u\n\n", 3000000000);
+
+	return 0; 
+}
